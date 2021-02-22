00093f962e4a6e53399f8702f9d85679a9ab2e6d
Hanneke <hanneke3108@gmail.com>
2021-02-22 11:29:58 +0100

    changed id names
    Deleted comments and changed id names.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var s = 75;
 
-document.getElementById('speed').innerHTML = s;
+document.getElementById('fuel').innerHTML = s;
 document.getElementById('balk').style.width = s+'%';
 
-document.getElementById('gas').onclick = function(){
+document.getElementById('tank').onclick = function(){
     s += 3;
 
     if(s > 100){
         s = 100;
     }
-    document.getElementById('speed').innerHTML = s;
+    document.getElementById('fuel').innerHTML = s;
     document.getElementById('balk').style.width = s+'%';
 };
