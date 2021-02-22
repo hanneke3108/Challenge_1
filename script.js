ebc8d68373d349767c5cfd3175aa29beed8c0955
Hanneke <hanneke3108@gmail.com>
2021-02-22 13:43:01 +0100

    personalizing values
    Changed values and names like rgb colours and months.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Chart 1: own barChart
+
 var s = 75;
 
 document.getElementById('fuel').innerHTML = s;
@@ -13,68 +15,70 @@ document.getElementById('tank').onclick = function(){
     document.getElementById('balk').style.width = s+'%';
 };
 
-//var myLineChart = new Chart(ctx, {
-//    type: 'line',
-//    data: data,
-//    options: options
-//});
+
+
+// Chart 2: js lineChart
 
 var chart1 = document.getElementById("lineChart");
+
+Chart.defaults.global.legend.labels.fontColor = "#fff";
 
 var lineChart = new Chart(chart1, {
     type: 'line',
     data: {
-        labels: ["1000", "2000", "3000", "4000", "5000"],
+        labels: ["March", "April", "May", "June", "July", "August", "September", "October"],
         datasets: [
             {
                 label: "Current speed",
-                fill: true,
-                lineTension: 0,
-                backgroundColor: "rgba(75, 192, 192, 0.4)",
-                borderColor: "rgba(75, 192, 192, 1)",
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75, 192, 192, 1)",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
-                pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+                fill: false,
+                lineTension: 0.3,
+                borderColor: "rgba(63, 215, 215, 1)",
+//                borderCapStyle: 'butt',
+//                borderDash: [],
+//                borderDashOffset: 0.0,
+//                borderJoinStyle: 'miter',
+//                pointBorderColor: "rgb(255, 255, 255)",
+//                pointBackgroundColor: "#fff",
+//                pointBorderWidth: 1,
+//                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgb(75, 192, 192)",
+                pointHoverBorderColor: "rgb(0, 0, 0)",
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
                 pointHitRadius: 10,
-                data: [65, 59, 80, 81, 56, 55, 40],
+                data: [1500, 2000, 2500, 2000, 3000, 3500, 3800, 3500],
             }, 
             {
                 label: "Speed from previous flight",
                 fill: true,
-                lineTension: 0,
-                backgroundColor: "rgba(75, 192, 192, 0.4)",
-                borderColor: "rgba(75, 192, 192, 1)",
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75, 192, 192, 1)",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
-                pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+                lineTension: 0.3,
+                backgroundColor: "rgba(255, 255, 255, 0.2)",
+                borderColor: "rgba(255, 255, 255, 1)",
+//                borderCapStyle: 'butt',
+//                borderDash: [],
+//                borderDashOffset: 0.0,
+//                borderJoinStyle: 'miter',
+//                pointBorderColor: "rgba(75, 192, 192, 1)",
+//                pointBackgroundColor: "#fff",
+//                pointBorderWidth: 1,
+//                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgb(255, 255, 255)",
+                pointHoverBorderColor: "rgb(0, 0, 0)",
                 pointHoverBorderWidth: 2,
                 pointRadius: 1,
                 pointHitRadius: 10,
-                data: [65, 59, 80, 81, 56, 55, 40],
+                data: [1500, 1500, 1800, 2500, 2500, 3000, 2500, 3500],
             }
         ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
     }
-//    options: {
-//        scales: {
-//            yAxes: [{
-//                stacked: true
-//            }]
-//        }
-//    }
+
 });
